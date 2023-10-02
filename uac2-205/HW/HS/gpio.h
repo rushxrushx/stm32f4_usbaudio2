6964acbed3c8abdcea21658d1ec17a8be09af373
rushxrushx <you@example.com>
2023-10-02 15:48:03 +0800

    v0.1

---
+++ uac2-205/HW/HS/gpio.h
@@ -0,0 +1,35 @@
+#ifndef __LED_H
+#define __LED_H
+#include "sys.h"
+
+//st 205	
+
+#define LED1 PAout(0)
+#define LED2 PAout(1)
+#define LED3 PAout(2)
+#define LED4 PAout(4)
+
+#define LEDON	LED1=0;LED2=1;
+#define LEDOFF	LED1=0;LED2=0;
+#define LED_YEL	LED1=1;LED2=1;
+#define LED_RED	LED1=1;LED2=0;
+
+#define LED2_GRN	LED3=0;LED4=1;
+#define LED2_YEL	LED3=1;LED4=1;
+#define LED2_RED	LED3=1;LED4=0;
+#define LED2_OFF	LED3=0;LED4=0;
+
+#define SEL_24	PBout(8)=0;PBout(9)=1;
+#define SEL_22	PBout(9)=0;PBout(8)=1;
+#define SEL_NONE	PBout(8)=0;PBout(9)=0;
+#define DAC_EN	PCout(10)=1;
+#define DAC_DIS	PCout(10)=0;
+#define APWR_STAGE1_EN	PBout(8)=1;
+#define APWR_STAGE1_DIS	PBout(8)=0;
+#define APWR_EN		PBout(9)=1;
+#define APWR_DIS	PBout(9)=0;
+    
+
+
+void Board_Init(void);//初始化		 				    
+#endif
